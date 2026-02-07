070f47f4cb1ae49ea487b6cdf03a8be6913a26f6
Swati Rout <swatipriyadarshineerout700@gmail.com>
2026-02-07 22:35:07 +0530

    Impliment Sentry Setup, Session Histroy

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://b86e7b9240ae306f6f587bfcba4defb5@o4510845022765056.ingest.de.sentry.io/4510845041311824",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
