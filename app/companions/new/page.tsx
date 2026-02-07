760098941e65b9570e05adb60cd408b9c968cc88
Swati Rout <swatipriyadarshineerout700@gmail.com>
2026-02-08 00:27:36 +0530

    Impliment My Journey Page

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,18 +1,41 @@
 import CompanionForm from "@/components/CompanionForm";
 import {auth} from "@clerk/nextjs/server";
 import {redirect} from "next/navigation";
+import {newCompanionPermission} from "@/lib/actions/companion.actions";
+import Image from "next/image";
+import Link from "next/link";
 
 const NewCompanion = async () => {
 	const { userId } = await auth();
 	if(!userId) redirect('/sign-in');
 
+	const canCreateCompanion = await newCompanionPermission();
+
 	return (
 		<main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-			<article className="w-full gap-4 flex flex-col">
-				<h1>Companion Builder</h1>
+			{canCreateCompanion ?
+				( <article className="w-full gap-4 flex flex-col">
+					<h1>Companion Builder</h1>
 
-				<CompanionForm/>
-			</article>
+					<CompanionForm/>
+				</article>
+				) :
+				(
+					<article className="companion-limit">
+						<Image src="/images/limit.svg" alt="Companion limit reached" width={360} height={360}/>
+						<div className="cta-badge">
+							Upgrade Your Plan
+						</div>
+						<h1>You've Reached Your Limit</h1>
+						<p>
+							you've reached your companion limit. Upgrade to
+							create more companions premium features.
+						</p>
+						<Link href="/subscription" className="btn-primary w-full justify-center">
+							Upgrade My Plan
+						</Link>
+					</article>
+				)} 
 		</main>
 	)
 }
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -74,7 +74,7 @@ export const getRecentSession = async (limit=10) => {
     return data.map(({ companions }) => companions);
 }
 
-export const getUserSession = async (userId: string, limit=10) => {
+export const getUserSessions = async (userId: string, limit=10) => {
     const supabase = createSupabaseClient();
     const { data, error } = await supabase
         .from('session_history')
@@ -84,4 +84,45 @@ export const getUserSession = async (userId: string, limit=10) => {
         .limit(limit)
     if(error) throw new Error (error.message);
     return data.map(({ companions }) => companions);
+}
+
+export const getUserCompanions = async (userId: string) => {
+    const supabase = createSupabaseClient();
+    const { data, error } = await supabase
+        .from('companions')
+        .select()
+        .eq('author', userId)
+
+    if(error) throw new Error (error.message);
+    return data;
+}
+
+export  const newCompanionPermission = async () => {
+    const { userId, has } = await auth();
+    const supabase = createSupabaseClient();
+
+    let limit = 0;
+
+    if (has({ plan: 'pro' })) {
+        return true;
+    } else if(has({ feature: "3_companion_limit" })){
+        limit = 3;
+    } else if(has({ feature: "10_companion_limit" })){
+        limit = 10;
+    }
+
+    const { data, error } = await supabase
+        .from('companions')
+        .select('id', { count: 'exact' })
+        .eq('author', userId)
+
+    if(error) throw new Error (error.message);
+
+    const companionCount = data?.length;
+
+    if(companionCount >= limit) {
+        return false;
+    } else {
+        return true;
+    }
 }
